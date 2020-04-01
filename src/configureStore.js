debf2ba359c22d6b16e0acc25991ca030600bd16
AswathyMohan-personal <aswathy.psm@gmail.com>
2020-04-01 16:16:12 +0530

    configured store

---
+++ src/configureStore.js
@@ -0,0 +1,7 @@
+import {creatStore,applyMiddleware, createStore} from "redux";
+import createSagaMiddleware from "redux-saga":
+
+export default configureStore=()=>{
+    const sagaMiddleware=createSagaMiddleware();
+    const store=createStore(applyMiddleware(sagaMiddleware));
+}
